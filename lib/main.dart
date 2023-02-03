3eb3c8cca05315c2e79d36c21c81d0c1c6afe359
alwihabsyi <alwihbsyi.ah@gmail.com>
2023-02-03 09:18:44 +0700

    2nd commit

--- lib/main.dart
+++ lib/main.dart
@@ -1,11 +1,11 @@
 import 'package:flutter/material.dart';
 
 void main() {
-  runApp(MyApp());
+  runApp(const MyApp());
 }
 
 class MyApp extends StatefulWidget {
-  MyApp({super.key});
+  const MyApp({super.key});
 
   @override
   State<MyApp> createState() => _MyAppState();
@@ -21,41 +21,52 @@ class _MyAppState extends State<MyApp> {
     return MaterialApp(
       debugShowCheckedModeBanner: false,
       home: Scaffold(
-        appBar: AppBar(
-          title: const Text("Feeling Flutter"),
-        ),
-        body: SizedBox(
-            width: double.infinity,
-            child: Column(
-              mainAxisAlignment: MainAxisAlignment.center,
-              crossAxisAlignment: CrossAxisAlignment.start,
-              children: [
-                ElevatedButton(
-                  onPressed: () {
-                    if (buttonName == 'Elevated Button in Flutter !!') {
-                      buttonName = 'U Clicked this';
-                    } else if (buttonName == 'U Clicked this') {
-                      buttonName = 'hey u press me again';
-                    } else {
-                      buttonName = 'that\'s it u can\'t press me now';
-                    }
-                    setState(() {
-                      buttonName = buttonName;
-                    });
-                  },
-                  child: Text(buttonName),
-                ),
-                ElevatedButton(
-                  onPressed: () {
-                    setState(() {
-                      buttonName = 'Elevated Button in Flutter !!';
-                    });
-                  },
-                  style: const ButtonStyle(),
-                  child: const Icon(Icons.refresh),
+        // appBar: AppBar(
+        //   title: const Text("Feeling Flutter"),
+        // ),
+        body: Center(
+          child: currentIndex == 0
+              ? Container(
+                  width: double.infinity,
+                  height: double.infinity,
+                  color: Colors.grey,
+                  child: Row(
+                    mainAxisAlignment: MainAxisAlignment.center,
+                    crossAxisAlignment: CrossAxisAlignment.center,
+                    children: [
+                      ElevatedButton(
+                        style: ElevatedButton.styleFrom(
+                            foregroundColor: Colors.white,
+                            backgroundColor: Colors.brown),
+                        onPressed: () {
+                          if (buttonName == 'Elevated Button in Flutter !!') {
+                            buttonName = 'U Clicked this';
+                          } else if (buttonName == 'U Clicked this') {
+                            buttonName = 'hey u press me again';
+                          } else {
+                            buttonName = 'that\'s it u can\'t press me now';
+                          }
+                          setState(() {
+                            buttonName = buttonName;
+                          });
+                        },
+                        child: Text(buttonName),
+                      ),
+                      ElevatedButton(
+                        onPressed: () {
+                          setState(() {
+                            buttonName = 'Elevated Button in Flutter !!';
+                            currentIndex = 1;
+                          });
+                        },
+                        style: const ButtonStyle(),
+                        child: const Icon(Icons.refresh),
+                      )
+                    ],
+                  ),
                 )
-              ],
-            )),
+              : Image.asset('images/shots.jpg'),
+        ),
         bottomNavigationBar: BottomNavigationBar(
           items: const [
             BottomNavigationBarItem(
